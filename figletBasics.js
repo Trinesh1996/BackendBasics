8d3fa6d5109c5ba18bcd07b5fea0e41dd5f795a7
Trinesh Chetty <trineshchetty@gmail.com>
2018-07-24 16:49:04 +0200

    node-require-figlet-servef

---
+++ figletBasics.js
@@ -0,0 +1,10 @@
+var figlet = require('figlet');
+ 
+figlet('Hello Why are tired today?', function(err, data) {
+    if (err) {
+        console.log('something went wrong, please check');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
